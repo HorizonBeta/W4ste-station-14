71d59e2d9abccc139a94892bda797a86312e66cc
Epic_guy33 <59932954+Epic-guy33@users.noreply.github.com>
2025-04-24 20:16:03 +0100

    Update RampingStationEventSchedulerComponent.cs
    Adjusted Timings

--- Content.Server/StationEvents/Components/RampingStationEventSchedulerComponent.cs
+++ Content.Server/StationEvents/Components/RampingStationEventSchedulerComponent.cs
@@ -10,14 +10,14 @@ public sealed partial class RampingStationEventSchedulerComponent : Component
     ///     Max chaos chosen for a round will deviate from this
     /// </summary>
     [DataField]
-    public float AverageChaos = 12f;
+    public float AverageChaos = 9f;
 
     /// <summary>
     ///     Average time (in minutes) for when the ramping event scheduler should stop increasing the chaos modifier.
     ///     Close to how long you expect a round to last, so you'll probably have to tweak this on downstreams.
     /// </summary>
     [DataField]
-    public float AverageEndTime = 90f;
+    public float AverageEndTime = 1800f;
 
     [DataField]
     public float EndTime;
